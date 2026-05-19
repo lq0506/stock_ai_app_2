5429274c8f5d1c333f1b901b6373ca1e5299baed
lq0506 <390576702@qq.com>
2026-05-19 10:30:20 +0800

    Update stock_ai_app_2.py

--- stock_ai_app_2.py
+++ stock_ai_app_2.py
@@ -172,7 +172,7 @@ def get_ma(row):
 # ===================== 手机APP界面 =====================
 def main():
     st.set_page_config(page_title="股票分析", page_icon="📈")
-    st.title("📈 金融指标股票分析")
+    st.title("📈 金融指标股票分析111111")
     st.divider()
 
     stock_code = st.text_input("请输入6位股票代码", value="603629")
